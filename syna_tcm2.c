66be22fc0ac3a1316db4f86143b0071186d42897
tbalden <illespal@gmail.com>
2022-10-20 18:30:33 +0000

    syn: uci: add screen state calls

--- syna_tcm2.c
+++ syna_tcm2.c
@@ -2095,6 +2095,10 @@ static void syna_check_finger_status(struct syna_tcm *tcm)
 		}
 	}
 }
+#ifdef CONFIG_UCI
+extern void uci_screen_state(int state);
+#endif
+
 
 /**
  * syna_dev_resume()
@@ -2196,6 +2200,10 @@ static int syna_dev_resume(struct device *dev)
 	retval = 0;
 
 	LOGI("Device resumed (pwr_state:%d)\n", tcm->pwr_state);
+#ifdef CONFIG_UCI
+	pr_info("%s uci screen state call %d... \n",__func__,2);
+	uci_screen_state(2);
+#endif
 exit:
 	/* set irq back to active mode if not enabled yet */
 	irq_enabled = (!hw_if->bdata_attn.irq_enabled);
@@ -2292,6 +2300,10 @@ static int syna_dev_suspend(struct device *dev)
 #endif
 
 	LOGI("Device suspended (pwr_state:%d)\n", tcm->pwr_state);
+#ifdef CONFIG_UCI
+	pr_info("%s uci screen state call %d... \n",__func__,0);
+	uci_screen_state(0);
+#endif
 
 	return 0;
 }
